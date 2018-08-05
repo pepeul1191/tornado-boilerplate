a8a25ebeca017e7405c8caee3bf517698158b7ff
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-04 23:31:16 -0500

    Agregando constatntes

---
+++ main/constants.py
@@ -0,0 +1,23 @@
+constants = {
+  'BASE_URL' : 'http://localhost:8888/',
+  'STATICS_URL' : 'http://localhost:8888/static/',
+  'CSRF' : 'mpt/sr6eS2AlCRHU7DVThMgFTN08pnfSDf/C94eZx7udfm0lvgaYWLYJttYPKzGKDTlXwVU/d2FOxbKkgNlsTw==',
+  'ambiente' : 'desarrollo',
+  'key' : 'Fdp6CVxpri1ga8kH',
+  'servicios' : {
+    'accesos' : 'http://localhost:5000/',
+    'agricultores' : 'http://localhost:3012/',
+    'ubicaciones' : 'http://localhost:3011/',
+    'archivos' : 'http://192.168.1.51:3031/',
+    'ftp' : {
+      'dominio' : '192.168.1.51',
+      'puerto' : 22,
+      'usuario' : 'pepe',
+      'contrasenia' : 'kiki123',
+      'ruta' : '/home/pepe/Documentos/py2/archivos/static/',
+      'public' : 'http://192.168.1.51:3031/'
+     },
+    'estaciones' : 'http://localhost:3025/',
+    'sensores' : 'http://localhost:3035/',
+  }
+}
